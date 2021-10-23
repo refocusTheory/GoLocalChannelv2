22b40ecf72843926f0ee64c044d3f422927eeeb3
refocus Theory <stilhausstudio@gmail.com>
2021-10-23 19:43:52 +0000

    v3.3

--- web/app/plugins/jquery-manager/index.php
+++
@@ -1,1 +0,0 @@
-<?php // Silence is golden, some servers are configured to show all the folders if there is no index file aka directory listings. This is to prevent that, security through obscurity.
--- web/app/plugins/jquery-manager/tests/php7.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Only possible in PHP 7
-define( 'WP_JQUERY_MANAGER_PLUGIN_JQUERY_SETTINGS', (array) get_option( 'wp_jquery_manager_plugin_jquery_settings' ) );
-define( 'WP_JQUERY_MANAGER_PLUGIN_JQUERY_MIGRATE_SETTINGS', (array) get_option( 'wp_jquery_manager_plugin_jquery_migrate_settings' ) );
